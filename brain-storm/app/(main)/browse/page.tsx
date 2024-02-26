9ef230607d5e535b3699bf9662d187c58b1714fe
reubenmoloney03@gmail.com <reubenmoloney03@gmail.com>
2024-02-26 10:16:10 +0000

    fixed an issue in browse page

--- brain-storm/app/(main)/browse/page.tsx
+++ brain-storm/app/(main)/browse/page.tsx
@@ -11,7 +11,7 @@ const browsePage = async () => {
             <div className="text-xl m-5">All of these groups are public and you can join them now!</div>
             <div className="">
             {groups.map((group) => (
-                <div className="flex flex-inline m-2 p-2 bg-zinc-400 rounded-md">{group.name} <button className="bg-zinc-200 p-1 rounded-lg hover:bg-zinc-50 ml-auto">Join</button></div>
+                <div key={group.id} className="flex flex-inline m-2 p-2 bg-zinc-400 rounded-md">{group.name} <button className="bg-zinc-200 p-1 rounded-lg hover:bg-zinc-50 ml-auto">Join</button></div>
             ))}
             </div>
         </div>
