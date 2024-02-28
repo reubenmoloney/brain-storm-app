45ae67edba55b20c3a292bd984851de0b1285d2f
reubenmoloney03@gmail.com <reubenmoloney03@gmail.com>
2024-02-28 19:15:31 +0000

    public group join maybe working now?

--- brain-storm/app/(main)/browse/page.tsx
+++ brain-storm/app/(main)/browse/page.tsx
@@ -1,17 +1,63 @@
 import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 
 const browsePage = async () => {
+    const profile = await currentProfile();
+
+    //if theres no profile then send to sign-in page
+    if(!profile) {
+        return redirectToSignIn();
+    }
     const groups = await db.group.findMany({
         where: {
             isPublic: true
         }
     })
+    const onClick = async (groupId: string) =>{
+        //need to check if user is in group before adding them -----------------
+        const alreadyInGroup = await db.group.findFirst({
+            where: {
+                id: groupId,
+                members:{
+                    some: {
+                        profileId: profile.id
+                    }
+                }
+            }
+        })
+
+        if(alreadyInGroup){
+            return null;
+        }
+
+
+        const updatedGroup = await db.group.update({
+            where: {
+                id: groupId,
+            },
+            data: {
+                members:{
+                    create:[
+                        {
+                            profileId: profile.id
+                        }
+                    ]
+                }
+            }
+        })
+        
+        //send to that group
+        return redirect(`/groups/${updatedGroup.id}`);
+    }
     return ( 
         <div>
             <div className="text-xl m-5">All of these groups are public and you can join them now!</div>
             <div className="">
             {groups.map((group) => (
-                <div key={group.id} className="flex flex-inline m-2 p-2 bg-zinc-400 rounded-md">{group.name} <button className="bg-zinc-200 p-1 rounded-lg hover:bg-zinc-50 ml-auto">Join</button></div>
+                <div key={group.id} className="flex flex-inline m-2 p-2 bg-zinc-400 rounded-md">{group.name} <button onClick={() =>onClick(group.id)}className="bg-zinc-200 p-1 rounded-lg hover:bg-zinc-50 ml-auto">Join</button></div>
             ))}
             </div>
         </div>
